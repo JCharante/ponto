fd496f245980889a11f0e3b257064d9547f8b253
JCharante <git@jcharante.com>
2017-10-18 10:39:28 -0400

    add database table schemas

---
+++ ponto/db/__init__.py
@@ -0,0 +1,29 @@
+from sqlalchemy import ForeignKey, Column, Integer, String, Boolean, create_engine, Text, DATETIME, JSON
+from sqlalchemy.dialects import mysql
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+import os
+
+Base = declarative_base()
+
+
+class UserV1(Base):
+	__tablename__ = 'UserV1'
+	user_id = Column(String(36), primary_key=True)
+	username = Column(Text(collation='utf8_general_ci'))
+	hashed_password = Column(Text(collation='utf8_general_ci'))
+
+
+class SessionV1(Base):
+	__tablename__ = 'SessionV1'
+	session_id = Column(String(36), primary_key=True)
+
+
+class PavlokKeyV1(Base):
+	__tablename__ = 'PavlokKeyV1'
+	key_id = Column(String(36), primary_key=True)
+	client_id = Column(String(64))
+	secret = Column(String(64))
+	callback_url = Column(Text())
+	access_token = Column(String(64))
+	refresh_token = Column(String(64))
