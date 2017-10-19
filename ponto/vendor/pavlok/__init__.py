cba0f9d7c7bab2e6ed431a4b72ba0ac8e789dc7b
John <git@jcharante.com>
2017-10-18 23:07:35 -0400

    add functions to make calls to all pavlok endpoints

--- ponto/vendor/pavlok/__init__.py
+++ ponto/vendor/pavlok/__init__.py
@@ -32,3 +32,106 @@ def get_access_token_and_refresh_token(
 	if r.status_code != 200:
 		raise exceptions.GenericError(response_data)
 	return response_data.get('access_token'), response_data.get('refresh_token'), int(response_data.get('expires_in'))
+
+
+def shock(
+	access_token: str,
+	value: int,
+	reason: str=None,
+) -> None:
+	"""
+	Sends a shock stimuli request to pavlok
+	:param access_token: acess_token required to authenticate with pavlok's api
+	:param value: Intensifier for the stimulus (1-255)
+	:param reason: Reason to show in the Pavlok App under logs.
+	:return:
+	"""
+	payload = {
+		"access_token": access_token,
+	}
+
+	if reason is not None:
+		payload['reason'] = reason
+
+	r = requests.post(f"{BASE_API_ENDPOINT}/api/v1/stimuli/shock/{value}", json=payload)
+	response_data = r.text
+	if r.status_code != 200:
+		raise exceptions.GenericError(response_data)
+
+
+def vibration(
+	access_token: str,
+	value: int,
+	reason: str=None
+) -> None:
+	"""
+	Sends a vibration stimuli request to pavlok
+	:param access_token:
+	:param value: The value intensifier for the stimulus (1-255)
+	:param reason:
+	:return:
+	"""
+
+	payload = {
+		"access_token": access_token,
+	}
+
+	if reason is not None:
+		payload['reason'] = reason
+
+	r = requests.post(f"{BASE_API_ENDPOINT}/api/v1/stimuli/vibration/{value}", json=payload)
+	response_data = r.text
+	if r.status_code != 200:
+		raise exceptions.GenericError(response_data)
+
+
+def led(
+	access_token: str,
+	value: int,
+	reason: str=None
+) -> None:
+	"""
+	Sends a LED stimulus to the associated pavlok
+	:param access_token:
+	:param value: The value intensifier for the stimulus (1-4)
+	:param reason:
+	:return:
+	"""
+
+	payload = {
+		"access_token": access_token,
+	}
+
+	if reason is not None:
+		payload['reason'] = reason
+
+	r = requests.post(f"{BASE_API_ENDPOINT}/api/v1/stimuli/led/{value}", json=payload)
+	response_data = r.text
+	if r.status_code != 200:
+		raise exceptions.GenericError(response_data)
+
+
+def beep(
+	access_token: str,
+	value: int,
+	reason: str=None
+) -> None:
+	"""
+	Sends a beep stimulus to the associated pavlok
+	:param access_token:
+	:param value: The value intensifier for the stimulus (1-4)
+	:param reason:
+	:return:
+	"""
+
+	payload = {
+		"access_token": access_token,
+	}
+
+	if reason is not None:
+		payload['reason'] = reason
+
+	r = requests.post(f"{BASE_API_ENDPOINT}/api/v1/stimuli/beep/{value}", json=payload)
+	response_data = r.text
+	if r.status_code != 200:
+		raise exceptions.GenericError(response_data)
